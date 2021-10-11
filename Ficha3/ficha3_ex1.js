8b4159f531287c2b42a2bc61c0d2f19ee6d215ab
Nuno Miguel <nm.albano20@gmail.com>
2021-10-11 23:09:33 +0100

    Ficha 3

--- Ficha3/ficha3_ex1.js
+++
@@ -1,8 +0,0 @@
-var num = parseInt(prompt("Insira um numero: "));
-if (num == 0) {
-        document.write("Nulo");
-} else if (num > 0) {
-        document.write("Positivo");
-} else {
-        document.write("Negativo");
-}
