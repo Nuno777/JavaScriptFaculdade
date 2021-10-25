c9efbe63af83dc9a26b5f8014f29c6ab2cf9b0c2
Nuno Miguel <nm.albano20@gmail.com>
2021-10-25 14:20:26 +0100

    Exercicio 4 concluido

--- Ficha5/ficha5_ex4.js
+++ Ficha5/ficha5_ex4.js
@@ -1,34 +1,40 @@
 let quit = true;
-var quanti = 0, dias = 0,soma=0;
+var quanti, dias, total = 0, totala = 0, totalb = 0, totalc = 0, totald = 0, totale = 0;
 do {
         var opcao = prompt("A - BTT ou Montanha\nB - Estrada\nC - Lazer\nD - Elétrica\nE - Criança\nS - Sair\n\nEscolha uma opção: ");
+        opcao = opcao.toUpperCase();
+        if (opcao >= 'A' && opcao <= 'E') {
+                dias = parseInt(prompt("Quantos dias de aluguer: "));
+        }
         switch (opcao) {
-                case 'a':
                 case 'A':
                         quanti = parseInt(prompt("Quantas bicicletas quer: "));
-                        dias = parseInt(prompt("Quantos dias de aluguer: "));
-                        dias=dias*12;
-                        soma=quanti+dias;
+                        totala = quanti * dias * 12;
                         break;
                 case 'B':
-                case 'b':
-
+                        quanti = parseInt(prompt("Quantas bicicletas quer: "));
+                        totalb = quanti * dias * 10;
                         break;
-                case 'c':
                 case 'C':
-
+                        quanti = parseInt(prompt("Quantas bicicletas quer: "));
+                        totalc = quanti * dias * 7.5;
                         break;
-                case 'd':
                 case 'D':
-
+                        quanti = parseInt(prompt("Quantas bicicletas quer: "));
+                        if (dias >= 4) {
+                                totald = 15 * quanti * 0.15 * dias;
+                        } else {
+                                totald = quanti * dias * 15;
+                        }
                         break;
-                case 'e':
                 case 'E':
+                        quanti = parseInt(prompt("Quantas bicicletas quer: "));
+                        totale = quanti * dias * 5;
                         break;
-                case 's':
                 case 'S':
                         quit = false;
-                        alert("Sair");
+                        total = totala + totalb + totalc + totald + totale;
+                        alert("Valor a pagar " + total);
                         break;
                 default:
                         alert("Opção não encontrada");
