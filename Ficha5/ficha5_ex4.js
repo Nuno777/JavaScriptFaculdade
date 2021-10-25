ed8b72ad25ee87eeb5c51ae2a66b3a9964588760
Nuno Miguel <nm.albano20@gmail.com>
2021-10-25 13:38:23 +0100

    Exercicio 4

--- Ficha5/ficha5_ex4.js
+++ Ficha5/ficha5_ex4.js
@@ -1,11 +1,14 @@
 let quit = true;
-var cont = 0;
+var quanti = 0, dias = 0,soma=0;
 do {
-        var opcao = prompt("R-Registar Pedido\nV-Valor a Pagar\nE-Dados Estatisticos\nT-Terminar\n\nEscolha uma opção: ");
+        var opcao = prompt("A - BTT ou Montanha\nB - Estrada\nC - Lazer\nD - Elétrica\nE - Criança\nS - Sair\n\nEscolha uma opção: ");
         switch (opcao) {
                 case 'a':
                 case 'A':
-
+                        quanti = parseInt(prompt("Quantas bicicletas quer: "));
+                        dias = parseInt(prompt("Quantos dias de aluguer: "));
+                        dias=dias*12;
+                        soma=quanti+dias;
                         break;
                 case 'B':
                 case 'b':
@@ -25,7 +28,7 @@ do {
                 case 's':
                 case 'S':
                         quit = false;
-                        alert("Opção de Sair");
+                        alert("Sair");
                         break;
                 default:
                         alert("Opção não encontrada");
