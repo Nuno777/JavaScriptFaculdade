aef079aed85ce9ded6de7a36f12095132adcd1db
Nuno Miguel <nm.albano20@gmail.com>
2021-11-15 16:43:57 +0000

    Acabar os exercicios da ficha 8

--- Ficha8/ficha8_ex2.js
+++ Ficha8/ficha8_ex2.js
@@ -1,19 +1,26 @@
 function lerNumero() {
     let sair = true;
     do {
-        i = parseInt(prompt("Insira um numero de 1 a 100: "));
-        for (let i = 1; i <= 100; i++) {
-            sair = false;
-            return i;
-        }
+        num = parseInt(prompt("Insira um numero de 1 a 100: "));
+        if (num < 1 || num > 100) {
 
+            sair = true;
+        }
+        else {
+            sair = false;
+            return num;
+
+        }
     } while (sair);
 }
 
-function calcSomatorio(somatorio) {
-
+function calcSomatorio() {
+    for (i = 0; i < num; i++) {
+        somatorio = num + i;
+    }
+    alert("Somatorio de " + num + " e " + somatorio);
     return somatorio;
 }
-
-i = lerNumero();
+ let num,somatorio;
+num = lerNumero();
 somatorio = calcSomatorio();
---
+++ Ficha8/ficha8_ex3.js
@@ -0,0 +1,6 @@
+function lerNumero(){
+    num=parseInt(prompt("Insira um numero: "));
+    if(num<1){
+
+    }
+}
