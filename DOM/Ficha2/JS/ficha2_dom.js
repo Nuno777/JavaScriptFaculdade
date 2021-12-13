9af165f8af9612e406130357f03e0de6a1636ac9
Nuno Miguel <nm.albano20@gmail.com>
2021-12-13 15:28:21 +0000

    Conclusão da ficha2

--- DOM/Ficha2/JS/ficha2_dom.js
+++ DOM/Ficha2/JS/ficha2_dom.js
@@ -1,22 +1,36 @@
-function mudarCor(cor){
-    let c=document.querySelector("#cor");
-    c.style.backgroundColor=cor;
+function mudarCor(cor) {
+    let c = document.querySelector("#cor");
+    c.style.backgroundColor = cor;
 }
 
-function addLista(){
+function addLista() {
     //criar elemento (li)
-   let x=document.createElement("li");
-   //inserir conteudo ao elemento(tag)
-   let y=document.getElementById("texto");
-   x.textContent=y.value; /*ou*/// x.innerHTML=y.Value;
-   //adicionar o elemento a um elemento (body)
-   let b=document.querySelector("ul");
-   b.appendChild(x);
+    let x = document.createElement("li");
+    //inserir conteudo ao elemento(tag)
+    let y = document.getElementById("texto");
+    x.textContent = y.value; /*ou*/// x.innerHTML=y.Value;
+    //adicionar o elemento a um elemento (body)
+    let b = document.querySelector("ul");
+    b.appendChild(x);
 }
 
-function limparLista(){
+function limparLista() {
     //vai selecionar o elemento
-    let x=document.querySelector("ul");
+    let x = document.querySelector("ul");
     //vai limpar e vai ficar vazio
-    x.textContent="";
+    x.textContent = "";
 }
+
+let index = 0;
+function carrocel() {
+    let x = document.querySelectorAll("div#ex3 img");
+    for (let i = index; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    index++;
+    if (index > (x.length - 1)) {
+        index = 0;
+    }
+    x[index].style.display = "block";
+}
+setInterval(carrocel, 1000);
